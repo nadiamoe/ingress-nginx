d55b132c0a47ba5b443394af503b37e15c12c739
Nadia Santalla <nadia@nadia.moe>
2026-05-27 12:59:26 +0200

    images/nginx: build.sh: update nginx to 1.31.1

--- images/nginx/rootfs/build.sh
+++ images/nginx/rootfs/build.sh
@@ -18,7 +18,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export NGINX_VERSION=1.27.1
+export NGINX_VERSION=1.31.1
 
 # Check for recent changes: https://github.com/vision5/ngx_devel_kit/compare/v0.3.3...master
 export NDK_VERSION=v0.3.3
@@ -188,7 +188,7 @@ mkdir --verbose -p "$BUILD_PATH"
 cd "$BUILD_PATH"
 
 # download, verify and extract the source files
-get_src 66dc7081488811e9f925719e34d1b4504c2801c81dee2920e5452a86b11405ae \
+get_src 9fcaaeb8f22544b09a19a761f3412c4112215422401634bebdd1296a403cc4bc \
   "https://nginx.org/download/nginx-$NGINX_VERSION.tar.gz"
 
 get_src aa961eafb8317e0eb8da37eb6e2c9ff42267edd18b56947384e719b85188f58b \
